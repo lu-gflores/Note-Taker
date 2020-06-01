3abdcb20c5c02c89f460aa372e0709cba2cd194d
George Flores <georgeflores886@gmail.com>
2020-06-01 15:33:53 -0400

    added app listener and routes

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express= require('express');
+const fs = require('fs');
 const app = express();
 const PORT = 8080;
 
+
+//routes
 app.get('/*', (req, res)=> {
     
 });
 
 app.get('/notes', (req, res) => {
-    
+
+})
+
+//PORT listener
+app.listen(PORT, function() {
+    console.log("Express App listening on PORT: " + PORT)
 })
