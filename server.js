92a0b388e10eb0b3ce862d897eb33aea112fd4a1
George Flores <georgeflores886@gmail.com>
2020-06-01 15:56:17 -0400

    added code in route and added express static

--- server.js
+++ server.js
@@ -3,14 +3,25 @@ const fs = require('fs');
 const app = express();
 const PORT = 8080;
 
+//serve static files in public folder
+app.use(express.static('public'));
+
 
 //routes
-app.get('/*', (req, res)=> {
-    
+app.get('*', (req, res)=> {
+    return fs.readFile(__dirname + '/public/index.html', (err, html) => {
+        if (err) throw err;
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(html);
+    })
 });
 
 app.get('/notes', (req, res) => {
-
+    return fs.readFile(__dirname + '/public/notes.html', (err, html) => {
+        if (err) throw err;
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(html);
+    })
 })
 
 //PORT listener
