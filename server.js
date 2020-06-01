328752ae0c53513c2d634bbc5419624d42ee604c
George Flores <georgeflores886@gmail.com>
2020-06-01 19:55:14 -0400

    added array and syntax to post

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const path = require('path')
 const app = express();
 const PORT = 8080;
+//may need for storing notes
+const userNotes = [];
 
 //serve static files in public folder
 app.use(express.static('public'));
@@ -10,29 +12,32 @@ app.use(express.static('public'));
 //handles data parsing
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
 //read db.json and return all saved notes as JSON
-app.get('/api/notes', (req, res) => {
-    
+app.get('/api/notes', function(req, res) {
+    // fs.readFile('db/db.json', 'utf8');
+    // res.json()
 })
 
 //add note to db.json and return note to user
-app.post('/api/notes', (req, res) => {
-
+app.post('/api/notes', function(req, res) {
+    let newNote = req.body; //save new note on body
+    userNotes.push(newNote);
+    res.json(newNote);
 })
 
 //remove note 
-app.delete('/api/notes/:id', (req, res) => {
-
+app.delete('/api/notes/:id', function(req, res) {
+    
 })
 
-//routes
-app.get('*', (req, res)=> {
-    res.sendfile(path.join(__dirname + '/public/index.html'))
+//html routes
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'))
 });
 
-app.get('/notes', (req, res) => {
-    res.sendfile(path.join(__dirname + "/public/notes.html"))
-
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(__dirname + "/public/notes.html"))
 })
 
 //PORT listener
