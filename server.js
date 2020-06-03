42ba9e93d5a63422b3a0070128e72a476706bc48
George Flores <georgeflores886@gmail.com>
2020-06-03 11:37:44 -0400

    reorganized html routes

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+
+//html routes
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'))
+});
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(__dirname + "/public/notes.html"))
+})
+
 //read db.json and return all saved notes as JSON
 app.get('/api/notes', function(req, res) {
     res.sendFile(path.join(__dirname, '/db/db.json'))
@@ -45,13 +54,7 @@ app.delete('/api/notes/:id', function(req, res) {
     res.json(userNotes);
 })
 
-//html routes
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'))
-});
-app.get('/notes', function(req, res) {
-    res.sendFile(path.join(__dirname + "/public/notes.html"))
-})
+
 
 //PORT listener
 app.listen(PORT, function() {
